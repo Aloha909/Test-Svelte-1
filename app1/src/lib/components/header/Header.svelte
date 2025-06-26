7660e2aac7553117479fce3c2a2f2a3e5f33e98e
Asmae TOUITI <a.touiti@groupeonepoint.com>
2025-06-26 13:51:30 +0200

    header

---
+++ app1/src/lib/components/header/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+  let title = $state("La ToDo App");
+  const handleClick = () => {
+    alert("Bouton cliqué !");
+    title = "La Todo App d'Alois & Asmae"
+  };
+</script>
+
+<header class="header">
+  <div class="container">
+    <h1>{title}</h1>
+    <button class="header-btn" onclick={handleClick}>
+      Changer titre
+    </button>
+  </div>
+</header>
+
+<style lang="scss"></style>
